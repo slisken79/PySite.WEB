342264c942c446b40a222bed727fac99ae77dbce
slisken79 <essadak@yahoo.se>
2023-10-17 16:32:42 +0200

    Lite ändring på start sida

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <HeaderComponent/>
       <BrowserRouter>
       <Routes>
-        <Route path="/home" element={<Home/>}></Route>
+        <Route path="/" element={<Home/>}></Route>
         <Route path="/about" element={<About/>}></Route>
         <Route path="*" element={<NotFound/>}></Route>
       </Routes>
